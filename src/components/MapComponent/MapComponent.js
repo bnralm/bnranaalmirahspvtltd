399a649f61d884f5f1b615e3b2e436108c9b4ce7
PankajRana <pankaj.rana@centivo.com>
2019-05-31 20:51:44 +0530

    sss

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 const Keys = require('./../../config/keys');
 
